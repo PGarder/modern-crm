efddc5d9d053cb54779393df661cb458186bb2c1
PGarder <psmith@gardnerweb.com>
2025-04-07 12:21:27 -0400

    Add Integrations component and reportWebVitals

---
+++ client/src/components/Integrations.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Integrations() {
+    return (
+        <div className="integrations">
+            <h2>Integrations</h2>
+            <div className="integrations-content">
+                <p>Manage your integrations here.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Integrations; 
